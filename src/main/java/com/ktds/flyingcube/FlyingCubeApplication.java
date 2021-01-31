660a3d7abb643cb189679b208c131dcbac016918
Lee Yu Pyeong <stoneberg73@gmail.com>
2021-01-31 20:43:08 +0900

    init commit

---
+++ src/main/java/com/ktds/flyingcube/FlyingCubeApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.flyingcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingCubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyingCubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktds/flyingcube/FlyingCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.flyingcube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingCubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
